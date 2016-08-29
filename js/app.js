04b64c07bcd117401279e76df2e918dde54b75a6
Jason Shafi <ocmc1503@gmail.com>
2016-08-29 14:23:29 -0700

    added a js randomizer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var allImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+var images [];
 
 function Image(imgName, imgPath) {
   this.imgName = imgName; //Name of the picture.
@@ -8,6 +9,13 @@ function Image(imgName, imgPath) {
   this.imageId = ''; //The ID to be stored when inserting the element into the DOM.
 };
 
-var imageOne = new Image(allImages[0].split('.jpg')[0], allImages[0]);
-var imageTwo = new Image(allImages[1].split('.jpg')[0], allImages[1]);
-var imageThree = new Image(allImages[2].split('.jpg'[0]), allImages[2]);
+function randomizes() {
+  for (var i = 0, i <3, i++) {
+    var randoNum (math.floor(math.random() * allImages.length));
+    images.push(new image(allImages[randoNum].split('.')[0], allImages[randoNum]));
+  }
+}
+
+var imageOne = new Image(allImages[0].split('.')[0], allImages[0]);
+var imageTwo = new Image(allImages[1].split('.')[0], allImages[1]);
+var imageThree = new Image(allImages[2].split('.'[0]), allImages[2]);
